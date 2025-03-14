9199137c05e5e07467e8afde3a15c7988a29b8ee
Felipe Magno de Almeida <felipe@expertise.dev>
2025-03-13 23:31:39 -0300

    A syntax error fix

--- custom_components/amt_alarms/__init__.py
+++ custom_components/amt_alarms/__init__.py
@@ -116,7 +116,7 @@ class AlarmHub:
 
     async def async_alarm_arm_mode(self, mode_list, code=None):
         """Send arm command with specific mode list"""
-        partitions = range(self.max_partitions)
+        partitions = [*range(self.max_partitions)]
         for i in partitions:
             if (mode_list[i] not in self.config_entry.data or
                 self.config_entry.data[mode_list[i]] != partition_on):
